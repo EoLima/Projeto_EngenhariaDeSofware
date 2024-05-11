04d0824dc49e6e6115f82526b464846d5a24005c
EoLima <lucasl.ima@icloud.com>
2024-05-10 22:00:36 -0300

    Deixando teclado e visual responsivo

--- FrontEnd/src/screens/singIn/index.js
+++ FrontEnd/src/screens/singIn/index.js
@@ -6,17 +6,16 @@ import {
   View,
   Image,
   TouchableOpacity,
+  TouchableWithoutFeedback,
 } from "react-native"
 import Checkbox from "expo-checkbox"
 import React, { useState } from "react"
 import { styles } from "./styles"
 import { useNavigation } from "@react-navigation/native"
-import IsKeyboardOpen from "../../components/IsKeyboardOpen"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 export default function App() {
   const [isChecked, setChecked] = useState(false)
-
-  const isKeyboardOpen = IsKeyboardOpen()
 
   const navigation = useNavigation()
 
@@ -27,71 +26,77 @@ export default function App() {
     }, 100)
   }
 
-  const handleTextInputFocus = () => {
-    // Verifica se o Teclado esta aberto, se tiver e ele clicar em cima de novo ele fecha
-    if (isKeyboardOpen == "open") {
-      Keyboard.dismiss()
-    }
-  }
-
   return (
-    <View style={styles.container}>
-      <View style={styles.containerLogo}>
-        <Image source={require("../../assets/logo.png")} style={styles.logo} />
-      </View>
+    <KeyboardAwareScrollView
+      behavior={"height"}
+    >
+      <TouchableWithoutFeedback
+        touchSoundDisabled
+        onPress={() => Keyboard.dismiss()}
+      >
+        <View style={styles.container}>
+          <View style={styles.containerLogo}>
+            <Image
+              source={require("../../assets/logo.png")}
+              style={styles.logo}
+            />
+          </View>
 
-      <View>
-        <Text style={styles.formTitle}>Digite o seu E-mail</Text>
-        <TextInput
-          style={styles.formInput}
-          onPressOut={handleTextInputFocus}
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoComplete="email"
-        />
+          <View>
+            <Text style={styles.formTitle}>Digite o seu E-mail</Text>
+            <TextInput
+              style={styles.formInput}
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoComplete="email"
+            />
 
-        <Text style={styles.formTitle}>Digite a sua Senha</Text>
-        <TextInput
-          style={styles.formInput}
-          onPressOut={handleTextInputFocus}
-          placeholder="Senha"
-          autoCapitalize="none"
-          secureTextEntry
-        />
-      </View>
+            <Text style={styles.formTitle}>Digite a sua Senha</Text>
+            <TextInput
+              style={styles.formInput}
+              placeholder="Senha"
+              autoCapitalize="none"
+              secureTextEntry
+            />
+          </View>
 
-      <View style={styles.formCheckBox}>
-        <View style={styles.subCheckBox}>
-          <Checkbox
-            style={styles.checkbox}
-            value={isChecked}
-            onValueChange={setChecked}
-            color={isChecked ? "#FF8A00" : "#D9D9D9"}
-          />
-          <Text style={styles.text}>Lembrar minha senha</Text>
+          <View style={styles.formCheckBox}>
+            <View style={styles.subCheckBox}>
+              <Checkbox
+                style={styles.checkbox}
+                value={isChecked}
+                onValueChange={setChecked}
+                color={isChecked ? "#FF8A00" : "#D9D9D9"}
+              />
+              <Text style={styles.text}>Lembrar minha senha</Text>
+            </View>
+            <TouchableOpacity>
+              <Text style={styles.text}>Esqueci minha senha</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.subForm}>
+            <TouchableOpacity style={styles.formButton1}>
+              <Text style={styles.textButton1}>ACESSAR</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.formButton2}
+              onPressOut={handlePress}
+            >
+              <Text style={styles.textButton2}>CRIAR CONTA</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.subFormCompany}>
+            <TouchableOpacity style={styles.formButtonCompany}>
+              <Text style={styles.textCompany}>Quem somos ?</Text>
+            </TouchableOpacity>
+          </View>
+
+          <StatusBar style="statusBar" />
         </View>
-        <TouchableOpacity>
-          <Text style={styles.text}>Esqueci minha senha</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.subForm}>
-        <TouchableOpacity style={styles.formButton1}>
-          <Text style={styles.textButton1}>ACESSAR</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.formButton2} onPressOut={handlePress}>
-          <Text style={styles.textButton2}>CRIAR CONTA</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.subFormCompany}>
-        <TouchableOpacity style={styles.formButtonCompany}>
-          <Text style={styles.textCompany}>Quem somos ?</Text>
-        </TouchableOpacity>
-      </View>
-
-      <StatusBar style="statusBar" />
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
   )
 }
--- FrontEnd/src/screens/singUp/index.js
+++ FrontEnd/src/screens/singUp/index.js
@@ -6,27 +6,19 @@ import {
   Text,
   TextInput,
   Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import { styles } from "./styles"
-import IsKeyboardOpen from "../../components/IsKeyboardOpen"
 import Checkbox from "expo-checkbox"
 import { TextInputMask } from "react-native-masked-text"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 export default function App() {
   const [image, setImage] = useState(null)
   const [isChecked, setChecked] = useState(false)
   const [cell, setCell] = useState("")
   const [birthdate, setBirthdate] = useState("")
-
-  const isKeyboardOpen = IsKeyboardOpen()
-
-  const handleTextInputFocus = () => {
-    // Verifica se o Teclado esta aberto, se tiver e ele clicar em cima de novo ele fecha
-    if (isKeyboardOpen == "open") {
-      Keyboard.dismiss()
-    }
-  }
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -42,145 +34,146 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <TouchableOpacity onPress={pickImage} style={styles.photoProfile}>
-          {image ? (
-            <Image source={{ uri: image }} style={styles.profileImage} />
-          ) : (
-            <View style={styles.standardProfile} />
-          )}
-        </TouchableOpacity>
-      </View>
+    <KeyboardAwareScrollView behavior={"height"}>
+      <TouchableWithoutFeedback
+        touchSoundDisabled
+        onPress={() => Keyboard.dismiss()}
+      >
+        <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <TouchableOpacity onPress={pickImage} style={styles.photoProfile}>
+              {image ? (
+                <Image source={{ uri: image }} style={styles.profileImage} />
+              ) : (
+                <View style={styles.standardProfile} />
+              )}
+            </TouchableOpacity>
+          </View>
 
-      <View style={styles.containerForm}>
-        <Text style={styles.formTitle}>Nome Completo</Text>
-        <View style={styles.containerNameFull}>
-          <TextInput
-            style={styles.formInput1}
-            onPressOut={handleTextInputFocus}
-            placeholder="Fulano"
-            keyboardType="default"
-            autoCapitalize="none"
-            autoComplete="given-name"
-          />
-          <TextInput
-            style={styles.formInput1}
-            onPressOut={handleTextInputFocus}
-            placeholder="Silva"
-            keyboardType="default"
-            autoCapitalize="none"
-            autoComplete="family-name"
-          />
-        </View>
+          <View style={styles.containerForm}>
+            <Text style={styles.formTitle}>Nome Completo</Text>
+            <View style={styles.containerNameFull}>
+              <TextInput
+                style={styles.formInput1}
+                placeholder="Fulano"
+                keyboardType="default"
+                autoCapitalize="none"
+                autoComplete="given-name"
+              />
+              <TextInput
+                style={styles.formInput1}
+                placeholder="Silva"
+                keyboardType="default"
+                autoCapitalize="none"
+                autoComplete="family-name"
+              />
+            </View>
 
-        <Text style={styles.formTitle}>E-mail</Text>
-        <TextInput
-          style={styles.formInput}
-          onPressOut={handleTextInputFocus}
-          placeholder="Exemplo@host.com.br"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoComplete="email"
-        />
-
-        <Text style={styles.formTitle}>Senha</Text>
-        <TextInput
-          style={styles.formInput}
-          onPressOut={handleTextInputFocus}
-          placeholder="****************"
-          autoCapitalize="none"
-          secureTextEntry
-        />
-
-        <Text style={styles.formTitle}>Celular</Text>
-        <TextInputMask
-          style={styles.formInput2}
-          onPressOut={handleTextInputFocus}
-          placeholder="( XX ) XXXXX-XXXX"
-          keyboardType="numeric"
-          autoCapitalize="none"
-          autoComplete="tel"
-          type={"cel-phone"}
-          options={{
-            maskType: "BRL",
-            withDDD: true,
-            dddMask: "(99) ",
-          }}
-          value={cell}
-          onChangeText={(text) => setCell(text)}
-        />
-
-        <Text style={styles.formTitle}>Data de Nascimento</Text>
-        <TextInputMask
-          style={styles.formInput2}
-          onPressOut={handleTextInputFocus}
-          placeholder="DD / MM / AAAA"
-          keyboardType="numeric"
-          autoCapitalize="none"
-          type="datetime"
-          options={{
-            format: "MM/DD/YYYY",
-          }}
-          value={birthdate}
-          onChangeText={(text) => setBirthdate(text)}
-        />
-      </View>
-
-      <View style={styles.formCheckBox}>
-        <View style={styles.subCheckBox}>
-          <Checkbox
-            style={styles.checkbox}
-            value={isChecked}
-            onValueChange={setChecked}
-            color={isChecked ? "#FF8A00" : "#D9D9D9"}
-          />
-        </View>
-        <Text style={styles.text}>
-          Li e estou de acordo com Termo de Uso e Politica de Privacidade
-        </Text>
-      </View>
-
-      <View style={styles.subForm}>
-        <TouchableOpacity style={styles.subFormButton}>
-          <Text style={styles.subTextButton}>CRIAR</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.subFormOtherAcess}>
-        <View style={styles.subFormChoiceAcess}>
-          <Image
-            source={require("../../assets/line.png")}
-            style={styles.line}
-          />
-          <Text style={styles.textSubFormAcess}>OU</Text>
-          <Image
-            source={require("../../assets/line.png")}
-            style={styles.line}
-          />
-        </View>
-
-        <View style={styles.containerLogo}>
-          <TouchableOpacity>
-            <Image
-              source={require("../../assets/face.png")}
-              style={styles.logo}
+            <Text style={styles.formTitle}>E-mail</Text>
+            <TextInput
+              style={styles.formInput}
+              placeholder="Exemplo@host.com.br"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoComplete="email"
             />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              source={require("../../assets/apple.png")}
-              style={styles.logo}
+
+            <Text style={styles.formTitle}>Senha</Text>
+            <TextInput
+              style={styles.formInput}
+              placeholder="****************"
+              autoCapitalize="none"
+              secureTextEntry
             />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              source={require("../../assets/google.png")}
-              style={styles.logo}
+
+            <Text style={styles.formTitle}>Celular</Text>
+            <TextInputMask
+              style={styles.formInput2}
+              placeholder="( XX ) XXXXX-XXXX"
+              keyboardType="numeric"
+              autoCapitalize="none"
+              autoComplete="tel"
+              type={"cel-phone"}
+              options={{
+                maskType: "BRL",
+                withDDD: true,
+                dddMask: "(99) ",
+              }}
+              value={cell}
+              onChangeText={(text) => setCell(text)}
             />
-          </TouchableOpacity>
+
+            <Text style={styles.formTitle}>Data de Nascimento</Text>
+            <TextInputMask
+              style={styles.formInput2}
+              placeholder="DD / MM / AAAA"
+              keyboardType="numeric"
+              autoCapitalize="none"
+              type="datetime"
+              options={{
+                format: "MM/DD/YYYY",
+              }}
+              value={birthdate}
+              onChangeText={(text) => setBirthdate(text)}
+            />
+          </View>
+
+          <View style={styles.formCheckBox}>
+            <View style={styles.subCheckBox}>
+              <Checkbox
+                style={styles.checkbox}
+                value={isChecked}
+                onValueChange={setChecked}
+                color={isChecked ? "#FF8A00" : "#D9D9D9"}
+              />
+            </View>
+            <Text style={styles.text}>
+              Li e estou de acordo com Termo de Uso e Politica de Privacidade
+            </Text>
+          </View>
+
+          <View style={styles.subForm}>
+            <TouchableOpacity style={styles.subFormButton}>
+              <Text style={styles.subTextButton}>CRIAR</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.subFormOtherAcess}>
+            <View style={styles.subFormChoiceAcess}>
+              <Image
+                source={require("../../assets/line.png")}
+                style={styles.line}
+              />
+              <Text style={styles.textSubFormAcess}>OU</Text>
+              <Image
+                source={require("../../assets/line.png")}
+                style={styles.line}
+              />
+            </View>
+
+            <View style={styles.containerLogo}>
+              <TouchableOpacity>
+                <Image
+                  source={require("../../assets/face.png")}
+                  style={styles.logo}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  source={require("../../assets/apple.png")}
+                  style={styles.logo}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  source={require("../../assets/google.png")}
+                  style={styles.logo}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
   )
 }
