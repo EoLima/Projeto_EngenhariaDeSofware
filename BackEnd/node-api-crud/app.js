ad8d348bc39c0c9ee0e86da593c6f9db64f4c361
Luís Fernando <114518986+lfernando-cn@users.noreply.github.com>
2024-05-15 09:52:07 -0300

    Update app.js

--- BackEnd/node-api-crud/app.js
+++ BackEnd/node-api-crud/app.js
@@ -20,7 +20,7 @@ const app = express();
   app.use(express.json());
   app.use('/api', userRoutes);
 
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ----> ${PORT}`);
   });
